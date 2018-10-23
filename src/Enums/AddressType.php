694b239489972922ae0689265229de679cbc8940
Roman Varkuta <roman.varkuta@gmail.com>
2018-10-23 16:51:59 +0300

    Implement address type enum (#45)

---
+++ src/Enums/AddressType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Wearesho\Pvbki\Enums;
+
+use Wearesho\Pvbki\Infrastructure\Enum;
+
+/**
+ * Class AddressType
+ * @package Wearesho\Pvbki\Enums
+ *
+ * @method static AddressType REGISTRATION()
+ * @method static AddressType FACTUAL()
+ * @method static AddressType POSTAL()
+ * @method static AddressType CURRENT_EMPLOYMENT()
+ * @method static AddressType PREVIOUS_EMPLOYMENT()
+ * @method static AddressType COLLATERAL()
+ */
+class AddressType extends Enum
+{
+    /**
+     * Address of permanent residence or registration
+     */
+    public const REGISTRATION = 1;
+
+    /**
+     * Factual address
+     */
+    public const FACTUAL = 2;
+
+    /**
+     * Postal address
+     */
+    public const POSTAL = 3;
+
+    /**
+     * Address of employment place
+     */
+    public const CURRENT_EMPLOYMENT = 4;
+
+    /**
+     * Address of previous employment place
+     */
+    public const PREVIOUS_EMPLOYMENT = 5;
+
+    /**
+     * Address of collateral
+     */
+    public const COLLATERAL = 6;
+}
