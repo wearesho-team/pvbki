d84a9adba6ea23608badc12e6da06124d6ef0c92
KartaviK <varkuta.roma@gmail.com>
2018-10-23 17:15:39 +0300

    Change abstract class to interface

---
+++ src/Interrelations/NullableEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wearesho\Pvbki\Interrelations;
+
+/**
+ * Interface NullableEnum
+ * @package Wearesho\Pvbki\Interrelations
+ *
+ * Used for Enums
+ */
+interface NullableEnum
+{
+    public const UNDEFINED = null;
+}
