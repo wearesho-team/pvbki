e758f8030e8fbb9f6a23e3bcab4145649509cdd2
Roman Varkuta <roman.varkuta@gmail.com>
2018-10-24 14:31:34 +0300

    Implement and integrate Gender enum (#57)

--- tests/Unit/Elements/SubjectTest.php
+++ tests/Unit/Elements/SubjectTest.php
@@ -16,7 +16,6 @@ class SubjectTest extends TestCase
 {
     protected const REQUEST_ID = 'request_id';
     protected const LAST_UPDATE = '2018-09-25';
-    protected const GENDER = 1;
     protected const SURNAME_UA = 'surname_ua';
     protected const SURNAME_RU = 'surname_ru';
     protected const SURNAME_EN = 'surname_en';
@@ -57,7 +56,7 @@ class SubjectTest extends TestCase
             static::REQUEST_ID,
             Carbon::parse(static::LAST_UPDATE),
             Pvbki\Enums\Entity::SUBJECT(),
-            static::GENDER,
+            Pvbki\Enums\Gender::MAN(),
             new Pvbki\Sentence\Translation(
                 static::SURNAME_UA,
                 static::SURNAME_RU,
@@ -115,7 +114,7 @@ class SubjectTest extends TestCase
                 'requestId' => static::REQUEST_ID,
                 'lastUpdate' => Carbon::parse(static::LAST_UPDATE),
                 'entity' => Pvbki\Enums\Entity::SUBJECT(),
-                'gender' => static::GENDER,
+                'gender' => Pvbki\Enums\Gender::MAN(),
                 'surname' => new Pvbki\Sentence\Translation(
                     static::SURNAME_UA,
                     static::SURNAME_RU,
@@ -275,7 +274,7 @@ class SubjectTest extends TestCase
 
     public function testGetGender(): void
     {
-        $this->assertEquals(static::GENDER, $this->fakeSubject->getGender());
+        $this->assertEquals(Pvbki\Enums\Gender::MAN(), $this->fakeSubject->getGender());
     }
 
     public function testGetEducation(): void
