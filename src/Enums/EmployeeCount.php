235475a4d495ba3a0b67acdc6c9a5b9babf3fe22
Roman Varkuta <varkuta.roma@gmail.com>
2018-10-24 14:10:24 +0300

    Fix enum values

--- src/Enums/EmployeeCount.php
+++ src/Enums/EmployeeCount.php
@@ -20,6 +20,6 @@ final class EmployeeCount extends Enum implements NullableEnum
     public const FROM_1_TO_5 = 1;
     public const FROM_6_TO_30 = 2;
     public const FROM_31_TO_100 = 3;
-    public const FROM_101_TO_500 = 3;
-    public const MORE_THAN_500 = 3;
+    public const FROM_101_TO_500 = 4;
+    public const MORE_THAN_500 = 5;
 }
