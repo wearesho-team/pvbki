4faac6e7ffa5dad87236188ecbc899f53075763a
KartaviK <varkuta.roma@gmail.com>
2018-10-26 16:48:41 +0300

    Revert changes for enum tests

--- tests/Unit/Infrastructure/EnumTest.php
+++ tests/Unit/Infrastructure/EnumTest.php
@@ -2,23 +2,25 @@
 
 namespace Wearesho\Pvbki\Tests\Unit\Infrastructure;
 
+use Wearesho\Pvbki\Infrastructure\Enum;
+
 use PHPUnit\Framework\TestCase;
-use Wearesho\Pvbki\Enums\Status;
 
 /**
  * Class EnumTest
  * @package Wearesho\Pvbki\Tests\Unit\Infrastructure
- * @coversDefaultClass \Wearesho\Pvbki\Infrastructure\Enum
+ * @coversDefaultClass Enum
  * @internal
  */
 class EnumTest extends TestCase
 {
     /**
      * @expectedException \BadMethodCallException
-     * @expectedExceptionMessage No static method or enum constant 'INVALID_ENUM' in class Wearesho\Pvbki\Enums\Status
+     * @expectedExceptionMessage No static method or enum constant 'INVALID_CONST' in class
+     *     Wearesho\Pvbki\Infrastructure\Enum
      */
     public function testBadMethodCall(): void
     {
-        Status::INVALID_ENUM();
+        Enum::INVALID_CONST();
     }
 }
