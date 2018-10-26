00f1aa384ead9ef70394b4acfbddded860aec9d9
KartaviK <varkuta.roma@gmail.com>
2018-10-26 16:14:27 +0300

    Few fixing dependant use cases and field type

--- src/Elements/Dependant.php
+++ src/Elements/Dependant.php
@@ -2,19 +2,19 @@
 
 namespace Wearesho\Pvbki\Elements;
 
-use Wearesho\Pvbki\Infrastructure\Element;
+use Wearesho\Pvbki;
 
 /**
  * Class Dependant
  * @package Wearesho\Pvbki\Elements
  */
-class Dependant extends Element
+class Dependant extends Pvbki\Infrastructure\Element
 {
     public const ROOT = 'Dependant';
     public const COUNT = 'count';
     public const TYPE_ID = 'typeId';
 
-    /** @var int|null */
+    /** @var int */
     protected $count;
 
     /** @var int|null */
@@ -26,7 +26,7 @@ class Dependant extends Element
         $this->typeId = $typeId;
     }
 
-    public function getCount(): ?int
+    public function getCount(): int
     {
         return $this->count;
     }
