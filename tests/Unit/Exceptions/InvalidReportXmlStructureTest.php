2094bb3e72009487c2c8fb8a3b981454f19120cd
KartaviK <varkuta.roma@gmail.com>
2018-10-29 13:59:04 +0200

    Refactor expected message for exception

--- tests/Unit/Exceptions/InvalidReportXmlStructureTest.php
+++ tests/Unit/Exceptions/InvalidReportXmlStructureTest.php
@@ -26,30 +26,13 @@ class InvalidReportXmlStructureTest extends TestCase
     public function testToString(): void
     {
         $this->assertEquals(
-            'Wearesho\Pvbki\Exceptions\InvalidReportXmlStructure: '
-            . 'Xml document have invalid structure in '
-            . '/home/kartavik/projects/pvbki/tests/Unit/Exceptions/InvalidReportXmlStructureTest.php:23
-Stack trace:
-#0 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/Framework/TestCase.php(840): '
-            . 'Wearesho\Pvbki\Tests\Unit\Exceptions\InvalidReportXmlStructureTest->setUp()
-#1 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/Framework/TestResult.php(665): '
-            . 'PHPUnit\Framework\TestCase->runBare()
-#2 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/Framework/TestCase.php(798): '
-            . 'PHPUnit\Framework\TestResult'
-            . '->run(Object(Wearesho\Pvbki\Tests\Unit\Exceptions\InvalidReportXmlStructureTest))
-#3 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/Framework/TestSuite.php(750): '
-            . 'PHPUnit\Framework\TestCase->run(Object(PHPUnit\Framework\TestResult))
-#4 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/Framework/TestSuite.php(750): '
-            . 'PHPUnit\Framework\TestSuite->run(Object(PHPUnit\Framework\TestResult))
-#5 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/TextUI/TestRunner.php(586): '
-            . 'PHPUnit\Framework\TestSuite->run(Object(PHPUnit\Framework\TestResult))
-#6 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/TextUI/Command.php(203): '
-            . 'PHPUnit\TextUI\TestRunner->doRun(Object(PHPUnit\Framework\TestSuite), Array, true)
-#7 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/src/TextUI/Command.php(159): '
-            . 'PHPUnit\TextUI\Command->run(Array, true)
-#8 /home/kartavik/projects/pvbki/vendor/phpunit/phpunit/phpunit(53): PHPUnit\TextUI\Command::main()
-#9 {main}
-XML: xml',
+            InvalidReportXmlStructure::class . ': '
+            . $this->fakeInvalidReportXmlStructure->getMessage() . ' in '
+            . $this->fakeInvalidReportXmlStructure->getFile() . ':'
+            . $this->fakeInvalidReportXmlStructure->getLine() . PHP_EOL
+            . 'Stack trace:' . PHP_EOL
+            . $this->fakeInvalidReportXmlStructure->getTraceAsString() . PHP_EOL
+            . 'XML: xml',
             $this->fakeInvalidReportXmlStructure->__toString()
         );
     }
